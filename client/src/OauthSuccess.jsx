aa58cc0c8670e58f8c5dbe0659bc8d4f25d7d051
Toko122 <toko.migine@gmail.com>
2025-08-07 21:15:50 +0400

    n

--- client/src/OauthSuccess.jsx
+++ client/src/OauthSuccess.jsx
@@ -1,27 +1,25 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const OauthSuccess = () => {
+const OAuthSuccess = () => {
   const navigate = useNavigate()
 
   useEffect(() => {
-    const params = new URLSearchParams(window.location.search)
-    const name = params.get('name')
-    const email = params.get('email')
+    const query = new URLSearchParams(window.location.search)
+    const name = query.get('name')
+    const email = query.get('email')
 
-    if (name && email) {
+    if (email) {
       localStorage.setItem('user', JSON.stringify({ name, email }))
       navigate('/')
-    } else {
-      navigate('/login')
     }
-  }, [])
+  }, [navigate])
 
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <h1 className='text-xl font-semibold'>Logging you in...</h1>
+    <div className="h-screen flex items-center justify-center">
+      <p className="text-xl">Logging in with Google...</p>
     </div>
   )
 }
 
-export default OauthSuccess
+export default OAuthSuccess
