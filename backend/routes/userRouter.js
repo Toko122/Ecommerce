8f183c6a8b67378bcede840e87593bf5b5aa1a36
Toko122 <toko.migine@gmail.com>
2025-08-07 21:30:58 +0400

    n

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { LoginUser, RegisterUser } = require('../controllers/userController')
+const {googleLogin} = require('../controllers/googleAuthController')
 
 router.post('/login', LoginUser)
 router.post('/register', RegisterUser)
-
+router.post('/google-login', googleLogin)
 module.exports = router
