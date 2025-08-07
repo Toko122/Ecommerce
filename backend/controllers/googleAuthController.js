c80098f4a245617c5656bd0386147d1c494ac194
Toko122 <toko.migine@gmail.com>
2025-08-07 21:53:16 +0400

    n

--- backend/controllers/googleAuthController.js
+++ backend/controllers/googleAuthController.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENTID,
