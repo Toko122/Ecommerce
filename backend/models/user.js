84ccc30884aa3cfd74168fe8b2f3f203f3afb286
Toko122 <toko.migine@gmail.com>
2025-08-07 21:54:30 +0400

    n

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const UserSchema = mongoose.Schema({
     email: {type: String, required: true, unique: true},
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+    username: {type: String, unique: true},
+    password: {type: String},
     googleId: {type: String, unique: true, sparse: true},
     resetToken: {type: String},
     resetTokenExpire: {type: Date}
