9f9de55df5bc8276e4206728edaf67a5c749b57b
Toko122 <toko.migine@gmail.com>
2025-08-07 21:11:56 +0400

    Update LoginComponent.jsx

--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -41,7 +41,7 @@ const LoginComponent = () => {
         <h1 className='font-semibold text-2xl'>Login</h1>
 
         <Link to="https://ecommerce-kboc.onrender.com/auth/google">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded">Login with Google</button>
+          <button type="button" className="bg-blue-500 text-white px-4 py-2 rounded">Login with Google</button>
          </Link>
 
         <input
